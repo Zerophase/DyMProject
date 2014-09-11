b943a4fb216d33f749c1781546a1ef30464290b8
Justin Terry <justin.terry@loop.colum.edu>
2014-09-10 22:51:49 -0500

    Physics-based movement script for player..
    It's broken as fuck but it's a start if we want to utilize Unity's
    physics engine and handle movement and calculations that way.  Could be
    useful later on when handling deltaTime with the Time Stop ability.

---
+++ DyM/Assets/Scripts/GameObjects/Movement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2 : MonoBehaviour {
+
+	// Basic Player Variables
+    public float speed = 50f;
+    private Vector3 movement;
+    private bool onGround;
+    public Vector3 jumpForce;
+    public bool doubleJump;
+
+    void Update() {
+        float joystickDirectionX = Input.GetAxis("Horizontal");
+        movement.x = joystickDirectionX * speed;
+    }
+	void FixedUpdate () {
+        rigidbody.velocity = movement;
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+	}
+
+    void Jump() {
+        if(onGround)
+        {
+            Debug.Log("Button? " + Input.GetButtonDown("Jump"));
+            Debug.Log("Grounded? " + onGround);
+            rigidbody.AddForce(jumpForce, ForceMode.VelocityChange);
+            onGround = false;
+        }
+    }
+
+    void OnCollisionEnter()
+    {
+        onGround = true;
+    }
+
+    void OnCollisionExit()
+    {
+        onGround = false;
+    }
+}
