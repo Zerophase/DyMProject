978f4bef61cd0f66f912fbc96352f26de5c22dea
callmezyos <alex.lueck@loop.colum.edu>
2015-02-03 01:10:18 -0600

    Added Enemies, Walls, Platforms, and Pick-Ups on levels 2 and 3.

---
+++ DyM/Assets/Scripts/UI/WinScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScreen : MonoBehaviour
+{
+	public GUISkin skin;
+    public Texture winScreenTexture;
+	
+	void Update () 
+	{
+		if(Input.anyKey)
+			Application.LoadLevel("main_menu");
+	}
+
+    void OnGUI()
+    {
+	    GUI.skin = skin;
+        GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),winScreenTexture);
+    }
+}
