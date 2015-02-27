b1c942e4547eaca9b313600fe20d60106a356207
callmezyos <alex.lueck@loop.colum.edu>
2015-02-26 18:52:01 -0600

    Slight Changes

---
+++ DyM/Assets/Scripts/UI/HealthMessage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthMessage 
+{
+    private int message;
+    public int Message {get {return message;}}
+	public HealthMessage(int message)
+    {
+        this.message = message;
+    }
+}
