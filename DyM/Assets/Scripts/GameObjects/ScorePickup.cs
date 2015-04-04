2d8e1d196ba21de0bef161c366b0ce58fba507e0
Justin Terry <justin.terry@loop.colum.edu>
2015-04-03 19:45:40 -0500

    Re-added prefabs hopefully it works.

--- DyM/Assets/Scripts/GameObjects/ScorePickup.cs
+++ DyM/Assets/Scripts/GameObjects/ScorePickup.cs
@@ -6,13 +6,16 @@ using Assets.Scripts.Utilities.Messaging;
 using Assets.Scripts.MediatorPattern;
 using Assets.Scripts.GameObjects;
 
-public class ScorePickup : PhysicsMediator
-{
-	int value = 50;
-	public void PickUp(GameObject player)
-	{
-		player.GetComponent<Player>().AddScore(value);
-		Debug.Log ("Score Picked Up");
-		Destroy(gameObject);
-	}
+namespace Assets.Scripts.GameObjects
+{    
+    public class ScorePickup : PhysicsMediator
+    {
+        public int value = 0;
+        public void PickUp(GameObject player)
+        {
+            player.GetComponent<Player>().AddScore(value);
+            Debug.Log("Score Picked Up");
+            Destroy(gameObject);
+        }
+    }
 }
