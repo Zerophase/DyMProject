c0dd28b5b3695badcf0fdc5914da1f8b5ed5e882
callmezyos <alex.lueck@loop.colum.edu>
2015-04-09 23:04:01 -0500

    SplashScreen now loads the Main Menu after finishing

---
+++ DyM/Assets/Splash Screen/SplashScreenLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenLoader : MonoBehaviour {
+
+	public float delayTime = 5;
+	public bool done = false;
+
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = delayTime;
+
+		StartCoroutine("SomeFunction");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer > 0)
+			return;
+		if (done)
+			Application.LoadLevel(0);
+	}
+
+	IEnumerator SomeFunction() {
+
+		yield return null;
+	}
+
+}
