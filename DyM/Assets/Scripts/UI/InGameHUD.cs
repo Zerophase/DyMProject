0bc2af23a4f5fe1a228187728e9761e4326368e3
CallMeZyos <alex.lueck@loop.colum.edu>
2015-04-14 17:24:58 -0500

    Edited some stuff on the splash screen

--- DyM/Assets/Scripts/UI/InGameHUD.cs
+++ DyM/Assets/Scripts/UI/InGameHUD.cs
@@ -1,52 +1,52 @@
-﻿using Assets.Scripts.Utilities.Messaging.Interfaces;
-using ModestTree.Zenject;
-using UnityEngine;
-using System.Collections;
-using Assets.Scripts.Utilities.Messaging;
-using Assets.Scripts.UI;
-using UnityEngine.UI;
+﻿//using Assets.Scripts.Utilities.Messaging.Interfaces;
+//using ModestTree.Zenject;
+//using UnityEngine;
+//using System.Collections;
+//using Assets.Scripts.Utilities.Messaging;
+//using Assets.Scripts.UI;
+//using UnityEngine.UI;
 
-public class InGameHUD : MonoBehaviour, IOwner
-{
-    [Inject]
-    private IEntityManager entityManager;
+//public class InGameHUD : MonoBehaviour, IOwner
+//{
+//    [Inject]
+//    private IEntityManager entityManager;
 
-	[Inject]
-	private IReceiver receiver;
-	public IReceiver Receiver
-	{
-		set { receiver = value; }
-	}
+//    [Inject]
+//    private IReceiver receiver;
+//    public IReceiver Receiver
+//    {
+//        set { receiver = value; }
+//    }
 
-    [Inject]
-    private IIds id;
+//    [Inject]
+//    private IIds id;
 
-	private Slider healthSlider;
-	private Slider timeSlider;
-	void Start ()
-	{
-		receiver.Owner = this;
-		receiver.SubScribe();
-        id.CreateId();
+//    private Slider healthSlider;
+//    private Slider timeSlider;
+//    void Start ()
+//    {
+//        receiver.Owner = this;
+//        receiver.SubScribe();
+//        id.CreateId();
 
-        entityManager.Add(Entities.HUD, id.ObjectId, this);
+//        entityManager.Add(Entities.HUD, id.ObjectId, this);
 
-		var healthBar = GameObject.Find("HPBar");
-		healthSlider = healthBar.GetComponent<Slider>();
+//        var healthBar = GameObject.Find("HPBar");
+//        healthSlider = healthBar.GetComponent<Slider>();
 
-		var timeBar = GameObject.Find("TPBar");
-		timeSlider = timeBar.GetComponent<Slider>();
-	}
+//        var timeBar = GameObject.Find("TPBar");
+//        timeSlider = timeBar.GetComponent<Slider>();
+//    }
 
-	public void Receive(ITelegram telegram)
-	{
-		if(telegram.Message is HealthMessage)
-		{
-			healthSlider.normalizedValue = (telegram.Message as HealthMessage).Message/300.0f;
-		}
-		else if (telegram.Message is AbilityMessage)
-		{
-			timeSlider.normalizedValue = (telegram.Message as AbilityMessage).Message/5f;
-		}
-	}
-}
+//    public void Receive(ITelegram telegram)
+//    {
+//        if(telegram.Message is HealthMessage)
+//        {
+//            healthSlider.normalizedValue = (telegram.Message as HealthMessage).Message/300.0f;
+//        }
+//        else if (telegram.Message is AbilityMessage)
+//        {
+//            timeSlider.normalizedValue = (telegram.Message as AbilityMessage).Message/5f;
+//        }
+//    }
+//}
