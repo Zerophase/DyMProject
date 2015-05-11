e82f22dbdbd784f0ec5f07f58f5d8d36f27ac8fe
callmezyos <alex.lueck@loop.colum.edu>
2015-05-11 09:40:00 -0500

    Added the OnTriggerEnter Script

---
+++ DyM/Assets/OnTriggerEnter.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function OnTriggerEnter (collision : Collider)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Find("MainCamera").GetComponent("SplineController").enabled=true;
+        }
+    }
